a05afcecdaaddd5426835ea81d54072ae6acde65
Juan Pablo Betancourt Maldonado <43021811+JPBMTV@users.noreply.github.com>
2020-02-10 02:13:24 -0500

    Updates

---
+++ precache-manifest.08b8da1f680c7f394dbf5e106167dec7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602ce5ed12e31c1136c9ff91b8119046",
+    "url": "/EasyU/index.html"
+  },
+  {
+    "revision": "1b863f41b81f02495e60",
+    "url": "/EasyU/static/css/main.40f644ea.chunk.css"
+  },
+  {
+    "revision": "9c657a0cef00e24597f5",
+    "url": "/EasyU/static/js/2.855c4152.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/EasyU/static/js/2.855c4152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b863f41b81f02495e60",
+    "url": "/EasyU/static/js/main.81afb0b0.chunk.js"
+  },
+  {
+    "revision": "8259e61aa265c60bfb03",
+    "url": "/EasyU/static/js/runtime-main.cff2c710.js"
+  },
+  {
+    "revision": "3092737b59d721408c4e27bea4d2db93",
+    "url": "/EasyU/static/media/blueback.3092737b.jpg"
+  },
+  {
+    "revision": "3327bab3b80fded85ce8e6a144702848",
+    "url": "/EasyU/static/media/camila.3327bab3.jpg"
+  },
+  {
+    "revision": "e2665d4fd2c3edf24a648b96e9459e0a",
+    "url": "/EasyU/static/media/easyu.e2665d4f.png"
+  },
+  {
+    "revision": "28232ff98051ddfb3e1a17a97cde077f",
+    "url": "/EasyU/static/media/publi_andes.28232ff9.jpg"
+  },
+  {
+    "revision": "f183f02a7cc23a82a21be4c356322eb2",
+    "url": "/EasyU/static/media/publi_jave.f183f02a.jpg"
+  },
+  {
+    "revision": "6c9b65e03a455ef71aa4b49901e2f9d4",
+    "url": "/EasyU/static/media/publi_sabana.6c9b65e0.jpg"
+  },
+  {
+    "revision": "33cf8df84956e274a3d033365bd84930",
+    "url": "/EasyU/static/media/sistemasback.33cf8df8.jpg"
+  },
+  {
+    "revision": "aabfa339fca80e88b7369364ba92cef6",
+    "url": "/EasyU/static/media/sistemaslogo.aabfa339.jpg"
+  },
+  {
+    "revision": "16677ec9c9ea825eea2d349aea0afeb8",
+    "url": "/EasyU/static/media/unback.16677ec9.png"
+  }
+]);
