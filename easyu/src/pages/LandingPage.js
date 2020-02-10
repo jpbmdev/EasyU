69b121b1f64996bb392c8434baa453eb4abbae3d
Juan Pablo Betancourt Maldonado <43021811+JPBMTV@users.noreply.github.com>
2020-02-09 20:40:33 -0500

    proyect cleaned

---
+++ easyu/src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = (props) => {
+    return (
+        <React.Fragment>
+            <div class="alert alert-primary" role="alert">
+                Hola
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default LandingPage;
